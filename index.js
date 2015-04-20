404c4e8e2a61c6f6ab7967d0b84f0049d5585c0a
Alex Houchens <gregoryx.houchens@intel.com>
2015-04-20 08:23:10 -0700

    Add css for sunburst

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'bf-sunburst'
+  name: 'bf-sunburst',
+
+  included: function(app, parentAddon) {
+    this._super.included(app, parentAddon);
+    app.import('vendor/bf-sunburst.css');
+  }
 };
